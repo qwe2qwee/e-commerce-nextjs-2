a69ea13235fc0a6539dfd461bdaab69859856207
Hossin <94375287+qwe2qwee@users.noreply.github.com>
2024-01-02 17:14:18 +0300

    some

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           <AdminBar />
           {/* @ts-expect-error */}
           <Header />
+          
           {children}
           {/* @ts-expect-error */}
           <Footer />
